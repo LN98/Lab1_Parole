d3d3abc4b84591fd28436d84a342f2227a383984
4G-IT <4G-IT@INFO1-PC01>
2019-05-15 13:06:27 +0200

    parole

--- src/parole/ParoleController.java
+++ src/parole/ParoleController.java
@@ -1,3 +1,7 @@
+/**
+ * Sample Skeleton for 'Parole.fxml' Controller Class
+ */
+
 package parole;
 
 import java.net.URL;
@@ -6,49 +10,54 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import parole.model.Parole;
 
 public class ParoleController {
+
+	Parole elenco = new Parole();
+
+	@FXML // ResourceBundle that was given to the FXMLLoader
+	private ResourceBundle resources;
+
+	@FXML // URL location of the FXML file that was given to the FXMLLoader
+	private URL location;
+
+	@FXML // fx:id="txtParola"
+	private TextField txtParola; // Value injected by FXMLLoader
+
+	@FXML // fx:id="btnInserisci"
+	private Button btnInserisci; // Value injected by FXMLLoader
+
+	@FXML // fx:id="txtResult"
+	private TextArea txtResult; // Value injected by FXMLLoader
+
+	@FXML // fx:id="btnReset"
+	private Button btnReset; // Value injected by FXMLLoader
+
+	@FXML // fx:id="listView"
+	private ListView<?> listView; // Value injected by FXMLLoader
+
+	@FXML
+	void doInsert(ActionEvent event) {
+		elenco.addParola(txtParola.getText().replaceAll("^[a-zA-Z]", ""));
+		
+	}
 	
-	Parole elenco ;
+	@FXML
+	void doReset(ActionEvent event) {
+		elenco.reset();
+	}
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
-    private ResourceBundle resources;
+	@FXML // This method is called by the FXMLLoader when initialization is complete
+	void initialize() {
+		assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'Parole.fxml'.";
+		assert btnInserisci != null : "fx:id=\"btnInserisci\" was not injected: check your FXML file 'Parole.fxml'.";
+		assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Parole.fxml'.";
+		assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Parole.fxml'.";
+		assert listView != null : "fx:id=\"listView\" was not injected: check your FXML file 'Parole.fxml'.";
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
-    private URL location;
-
-    @FXML // fx:id="txtParola"
-    private TextField txtParola; // Value injected by FXMLLoader
-
-    @FXML // fx:id="txtResult"
-    private TextArea txtResult; // Value injected by FXMLLoader
-    
-    @FXML
-    private Button btnReset;
-
-    @FXML // fx:id="btnInserisci"
-    private Button btnInserisci; // Value injected by FXMLLoader
-
-    @FXML
-    void doInsert(ActionEvent event) {
-    	// TODO
-    }
-    
-    @FXML
-    void doReset(ActionEvent event) {
-    	// TODO
-    }
-
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
-        assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'Parole.fxml'.";
-        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Parole.fxml'.";
-        assert btnInserisci != null : "fx:id=\"btnInserisci\" was not injected: check your FXML file 'Parole.fxml'.";
-
-        elenco = new Parole() ;
-        
-    }
+	}
 }
--- src/parole/model/Parole.java
+++ src/parole/model/Parole.java
@@ -2,23 +2,48 @@ package parole.model;
 
 import java.util.List;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.ListView;
+
 public class Parole {
-		
+	
+	ListView<String> list = new ListView<String>();
+	ObservableList<String> items =FXCollections.observableArrayList();
+	
 	public Parole() {
-		//TODO
 	}
 	
 	public void addParola(String p) {
-		//TODO
+		
+		items.add(p);
+		list.setItems(items);
 	}
 	
-	public List<String> getElenco() {
-		//TODO
-		return null;
+	public ListView<String> getElenco() {
+		
+		return list;
 	}
 	
 	public void reset() {
-		// TODO
+		items.clear();
+		list.setItems(items);
 	}
+	
+	public void elimina(String p) {
+		
+	}
+
+	@Override
+	public String toString() {
+		return "Parole [list=" + list + ", items=" + items + "]";
+	}
+
+	public ObservableList<String> getItems() {
+		return items;
+	}
+	
+	
+	
 
 }
